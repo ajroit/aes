8c3e41c8d55db48b4fe23e47cba0c8f61805e68d
ajroit <128423805+ajroit@users.noreply.github.com>
2025-01-09 15:57:39 -0300

    Add files via upload

--- js/contacto.js
+++ js/contacto.js
@@ -2,27 +2,27 @@ document.addEventListener('DOMContentLoaded', function() {
     const form = document.getElementById('contact-form');
     const formMessage = document.getElementById('form-message');
     const chatButton = document.getElementById('open-chat');
-    
-      if(chatButton){
-           chatButton.addEventListener('click', function() {
-               window.open('chat.html', '_blank');
-          });
-      }
-
+   
+    if(chatButton){
+        chatButton.addEventListener('click', function() {
+            window.open('chat.html', '_blank');
+        });
+    }
+   
     if (form) {
       form.addEventListener('submit', async (e) => {
           e.preventDefault();
-
+   
           const formData = new FormData(form);
             try {
-                const response = await fetch('https://formulario-aes.ajroit-wa.workers.dev/', {  // Asegúrate que coincida la ruta de tu worker
+                const response = await fetch('/form-handler', {  // Asegúrate que coincida la ruta de tu worker
                     method: 'POST',
-					headers: {
-						'Content-Type': 'application/x-www-form-urlencoded'
-					},
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
                     body: new URLSearchParams(formData).toString(),
                 });
-
+   
                 const data = await response.json();
                 if (response.ok) {
                     formMessage.textContent = data.message;
@@ -42,4 +42,4 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     }
-});
+   });
